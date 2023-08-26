c935ef5b6ac188734975fe3eed9f5b7e65c50480
Al-Mansori <11410120210729@stud.cu.edu.eg>
2023-08-26 21:25:27 +0300

    uplode

---
+++ src/components/Note.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+
+function Note(props) {
+
+
+  function deleteIt(note) {
+    props.onDelete(props.id);
+  }
+
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button onClick={deleteIt}>DELETE</button>
+    </div>
+  );
+}
+
+export default Note;
